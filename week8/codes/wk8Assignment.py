288974373fc30876f66f067f6e4e6bc4e5ed8608
doimiod <doim@tcd.ie>
2022-11-03 15:06:34 +0000

    did q (i) (a)

---
+++ week8/codes/wk8Assignment.py
@@ -0,0 +1,67 @@
+from cmath import sqrt
+import numpy as np
+import math
+
+# make a function that takes an n × n array and a k × k kernel, convolves the kernel to the input array and returns the result
+def convolve(array, kernel):
+    if (array.size<kernel.size): # array should be larger than kernel
+        return 0
+    convolved = []
+    sum = 0
+    size = 0
+    kSize = math.sqrt(kernel.size)
+    kSize = int(kSize)
+    arrSize = math.sqrt(array.size)
+    arrSize = int(arrSize)
+    size = arrSize - kSize + 1
+    print(size)
+    for i in range(size):
+        for j in range(size):
+            for k in range(kSize):
+                for l in range(kSize):
+                    sum = sum + array[k+i,l+j]*kernel[k,l]
+            convolved.append(sum)
+            sum = 0   
+ 
+    convolved = np.array(convolved)
+    convoSize = math.sqrt(convolved.size)
+    convoSize = int(convoSize)
+    convolved = convolved.reshape(-1, convoSize)         
+    return convolved
+
+array = [1,0,1,0,-1,1,0,1,0,-1,1,0,1,0,-1,1,0,1,0,-1,1,0,1,0,-1]
+kernel1 = [1,2,1,1,1,3,2,1,1]
+array = np.array(array)
+array = array.reshape(-1,5)
+kernel1 = np.array(kernel1)
+kernel1 = kernel1.reshape(-1,3)
+print(array)
+print(kernel1)
+newArr1 = convolve(array, kernel1)
+print(newArr1)
+
+kernel2 = [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,2]
+kernel2 = np.array(kernel2)
+kernel2 = kernel2.reshape(-1,4)
+print(kernel2)
+
+newArr2 = convolve(array, kernel2)
+print(newArr2)
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                # sum = sum + array[j+i,k]*kernel[j,k]
+                # sum = sum + array[j,k+1]*kernel[j,k]
