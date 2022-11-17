18029a0074104842e28963966d595129bf5dee35
doimiod <doim@tcd.ie>
2022-11-17 01:52:02 +0000

    finihed q (i)

--- week8/codes/wk8qi.py
+++ week8/codes/wk8qi.py
@@ -7,12 +7,12 @@ from PIL import Image
 def convolve(array, kernel):
     if (array.size<kernel.size): # array should be larger than kernel
         return 0
-    convolved = []
+    convolved = []               # array for convolved img
     sum = 0
     size = 0
-    kSize = math.sqrt(kernel.size)
+    kSize = math.sqrt(kernel.size)  # kernel size. if the kernel is 3*3, the size is 3
     kSize = int(kSize)
-    arrSize = math.sqrt(array.size)
+    arrSize = math.sqrt(array.size) # priginal array size. if it is 3*3, the size is 3
     arrSize = int(arrSize)
     size = arrSize - kSize + 1
     print(size)
@@ -22,13 +22,15 @@ def convolve(array, kernel):
                 for l in range(kSize):
                     sum = sum + array[k+i,l+j]*kernel[k,l]
             convolved.append(sum)
-            sum = 0   
- 
+            sum = 0
+     
     convolved = np.array(convolved)
     convoSize = math.sqrt(convolved.size)
     convoSize = int(convoSize)
     convolved = convolved.reshape(-1, convoSize)         
     return convolved
+
+# the comment below was used to check if the conv function works correctly
 
 # array = [1,0,1,0,-1,1,0,1,0,-1,1,0,1,0,-1,1,0,1,0,-1,1,0,1,0,-1]
 # kernel1 = [1,2,1,1,1,3,2,1,1]
@@ -49,7 +51,7 @@ def convolve(array, kernel):
 # newArr2 = convolve(array, kernel2)
 # print(newArr2)
 
-im = Image.open("/Users/doimasanari/Desktop/ML/week8/pics.jpg/wk8.jpg")
+im = Image.open("/Users/doimasanari/Documents/ML/week8/pics.jpg/wk8.jpg")
 rgb = np.array(im.convert("RGB"))
 r=rgb[:,:,0] # array of R pixels 
 
@@ -67,7 +69,6 @@ kernel2 = np.array(kernel2)
 kernel2 = kernel2.reshape(-1,3)
 convolvedR = convolve(r, kernel2)
 Image.fromarray(np.uint8(convolvedR)).show()
-                
-                
+    
 # sum = sum + array[j+i,k]*kernel[j,k]
 # sum = sum + array[j,k+1]*kernel[j,k]
